0201400d6487688187eba952bc5945713d72fdf6
김영은 <95126m@naver.com>
2024-09-12 23:56:24 +0900

    modal 파일을 common 폴더로 이동

--- src/pages/AddPlaylist.tsx
+++ src/pages/AddPlaylist.tsx
@@ -1,6 +1,9 @@
-import Header from '@/components/theLayout/theHeader/Header'
-import Modal from '@/components/theLayout/Modal'
 import React, { useState } from 'react'
+import Header from '@/components/theLayout/theHeader/Header'
+import Category from '@/components/theCommon/Category'
+import { css } from '@emotion/react'
+import { Colors, FontSize, FontWeight } from '@/styles/Theme'
+import { GrAddCircle } from "react-icons/gr" 
 
 function Radio({ children, name, value, defaultChecked }) {
   return (
@@ -11,48 +14,73 @@ function Radio({ children, name, value, defaultChecked }) {
   )
 }
 const AddPlaylist = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const openModal = () => {
-    setIsModalOpen(true)
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false)
-  };
-
-  const deletePlaylist = () => {
-    setIsModalOpen(false);
-  }
   return (
     <>
       <Header title="Add Playlist" />
-      <div className='add-link'> 동영상 링크
-        <input></input>
-        <button></button>
+      <div css={DivContainer}>
+        <div css={TitleContainer}>동영상 링크</div>
+        <input type="text" placeholder="동영상 주소를 입력해주세요." css={InputContainer} />
+        <button css={AddButton}><GrAddCircle size={24} color="#1E1E1E" /></button>
       </div>
 
-      <div className='add-title'>플레이리스트 제목
-        <input type="text" placeholder="제목을 입력해주세요."/>
+      <div css={DivContainer}>
+        <div css={TitleContainer}>플레이리스트 제목</div>
+        <input type="text" placeholder="제목을 입력해주세요." css={InputContainer} />
       </div>
 
-      <div className='add-description'>설명
-        <input type="text" placeholder="설명을 입력해주세요."/>
+      <div css={DivContainer}>
+        <div css={TitleContainer}>설명</div>
+        <input type="text" placeholder="설명을 입력해주세요." css={InputContainer} />
       </div>
 
-      <div className='category-setting'>카테고리 설정
-        <input />
+      <div css={DivContainer}>
+        <div css={TitleContainer}>카테고리 설정</div>
+        <Category />
       </div>
 
-      <div className='open-setting'>공개 설정
+      <div css={DivContainer}>
+        <div css={TitleContainer}>공개 설정</div>
         <Radio name="setting" defaultChecked>공개</Radio>
         <Radio name="setting">비공개</Radio>
       </div>
-
-      <button onClick={openModal}>삭제</button>
-      <Modal isOpen={isModalOpen} onClose={closeModal} onDelete={deletePlaylist}></Modal>
     </>
   )
 }
+
+const DivContainer = css`
+  position: relative;
+  display: inline-block;
+  width: 315px;
+`
+const TitleContainer = css`
+  font-size: ${FontSize.md};
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`
+const InputContainer = css`
+  border: none;
+  border-bottom: 2px solid #EBEBEB;
+  outline: none;
+  font-size: ${FontSize.md};
+  font-weight: ${FontWeight.medium};
+  color: ${Colors.black};
+  height: 30px;
+  width: 100%;
+  letter-spacing: -0.2px;
+  padding-left: 10px;
+  padding-right: 40px;
+  box-sizing: border-box;
+  margin-bottom: 35px;
+`
+const AddButton = css`
+  position: absolute;
+  right: 0;
+  bottom: 50px;
+  transform: translateY(50%);
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`
 
 export default AddPlaylist;
