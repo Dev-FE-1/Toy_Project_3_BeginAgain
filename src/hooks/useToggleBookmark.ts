d2fc3e0a1e94072c5d230b166d05671f2f22870e
Panda-racoon <millie3519@gmail.com>
2024-09-17 01:09:28 +0900

    북마크 필터링으로 페이지구현

--- src/hooks/useToggleBookmark.ts
+++ src/hooks/useToggleBookmark.ts
@@ -1,48 +1,6 @@
-// import { useState, useEffect } from 'react'
-// import { getAuth } from 'firebase/auth'
-
-// // 북마크 참조용으로 사용되는 전역 상태 혹은 저장소
-// const bookmarkRefs: { [key: string]: string[] } = {}
-
-// export const useToggleBookmark = (playlistId: string) => {
-//   const [isBookmarked, setIsBookmarked] = useState(false)
-//   const auth = getAuth()
-//   const user = auth.currentUser
-
-//   useEffect(() => {
-//     if (!user) return
-
-//     // 북마크 참조에서 현재 사용자의 북마크 목록을 확인
-//     const userBookmarks = bookmarkRefs[user.uid] || []
-//     setIsBookmarked(userBookmarks.includes(playlistId))
-//   }, [playlistId, user])
-
-//   const toggleBookmark = () => {
-//     if (!user) return
-
-//     // 현재 사용자의 북마크 목록을 불러옴
-//     const userBookmarks = bookmarkRefs[user.uid] || []
-//     let updatedBookmarks
-
-//     if (isBookmarked) {
-//       // 이미 북마크된 경우 -> 북마크 해제
-//       updatedBookmarks = userBookmarks.filter(id => id !== playlistId)
-//     } else {
-//       // 북마크되지 않은 경우 -> 북마크 추가
-//       updatedBookmarks = [...userBookmarks, playlistId]
-//     }
-
-//     // 북마크 참조를 업데이트
-//     bookmarkRefs[user.uid] = updatedBookmarks
-//     setIsBookmarked(!isBookmarked)
-//   }
-
-//   return { isBookmarked, toggleBookmark }
-// }
-
 import { useState, useEffect } from 'react'
 import { getAuth } from 'firebase/auth'
-import { useBookmarkStore } from '@/stores/useBookmarkStore'
+import { useBookmarkStore } from '@/stores/useBookmark'
 
 export const useToggleBookmark = (playlistId: string) => {
   const [isBookmarked, setIsBookmarked] = useState(false)
