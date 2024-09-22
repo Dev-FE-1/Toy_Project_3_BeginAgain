8bea147cf416875b2401dd83863ee1e8e366dfd7
Panda-racoon <millie3519@gmail.com>
2024-09-23 04:56:05 +0900

    디테일페이지DnD구현(내플리만적용,전체플리적용 둘다 가지고 있음)

--- src/hooks/useFetchPlaylist.ts
+++ src/hooks/useFetchPlaylist.ts
@@ -1,8 +1,38 @@
-// GET - useQuery
-// POST, PUT, PATCH, DELETE - useMutation
+// // GET - useQuery
+// // POST, PUT, PATCH, DELETE - useMutation
+
+// import { useQuery } from '@tanstack/react-query'
+// import { getFirestore, collection, doc, getDoc } from 'firebase/firestore'
+
+// export interface Playlist {
+//   id: string
+//   urls: string[]
+//   title: string
+//   description: string
+//   isPublic: boolean
+//   categories: string[]
+//   userId: string
+//   createdAt: Date | string | undefined
+// }
+
+// export const useFetchPlaylist = (id: string) => {
+//   return useQuery<Playlist>({
+//     queryKey: ['playlists', id],
+//     queryFn: async () => {
+//       const db = getFirestore()
+//       const coll = collection(db, 'Playlists')
+//       const docRef = doc(coll, id)
+//       const docSnap = await getDoc(docRef)
+//       return {
+//         id: docSnap.id,
+//         ...docSnap.data()
+//       } as Playlist
+//     }
+//   })
+// }
 
 import { useQuery } from '@tanstack/react-query'
-import { getFirestore, collection, doc, getDoc } from 'firebase/firestore'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
 
 export interface Playlist {
   id: string
@@ -19,14 +49,21 @@ export const useFetchPlaylist = (id: string) => {
   return useQuery<Playlist>({
     queryKey: ['playlists', id],
     queryFn: async () => {
+      console.log('useFetchPlaylist 호출:', id)
       const db = getFirestore()
-      const coll = collection(db, 'Playlists')
-      const docRef = doc(coll, id)
+      const docRef = doc(db, 'Playlists', id)
       const docSnap = await getDoc(docRef)
-      return {
-        id: docSnap.id,
-        ...docSnap.data()
-      } as Playlist
+
+      if (docSnap.exists()) {
+        console.log('Firestore에서 가져온 데이터:', docSnap.data())
+        return {
+          id: docSnap.id,
+          ...docSnap.data()
+        } as Playlist
+      } else {
+        console.log('데이터가 존재하지 않습니다.')
+        throw new Error('해당 플레이리스트를 찾을 수 없습니다.')
+      }
     }
   })
 }
