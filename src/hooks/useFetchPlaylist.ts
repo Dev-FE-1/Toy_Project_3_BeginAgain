e126dd8dca4fd04b78affacfc94139191e0ee108
Panda-racoon <millie3519@gmail.com>
2024-09-23 14:26:52 +0900

    DnD작업중 디벨로 머지함

--- src/hooks/useFetchPlaylist.ts
+++ src/hooks/useFetchPlaylist.ts
@@ -1,36 +1,3 @@
-// // GET - useQuery
-// // POST, PUT, PATCH, DELETE - useMutation
-
-// import { useQuery } from '@tanstack/react-query'
-// import { getFirestore, collection, doc, getDoc } from 'firebase/firestore'
-
-// export interface Playlist {
-//   id: string
-//   urls: string[]
-//   title: string
-//   description: string
-//   isPublic: boolean
-//   categories: string[]
-//   userId: string
-//   createdAt: Date | string | undefined
-// }
-
-// export const useFetchPlaylist = (id: string) => {
-//   return useQuery<Playlist>({
-//     queryKey: ['playlists', id],
-//     queryFn: async () => {
-//       const db = getFirestore()
-//       const coll = collection(db, 'Playlists')
-//       const docRef = doc(coll, id)
-//       const docSnap = await getDoc(docRef)
-//       return {
-//         id: docSnap.id,
-//         ...docSnap.data()
-//       } as Playlist
-//     }
-//   })
-// }
-
 import { useQuery } from '@tanstack/react-query'
 import { getFirestore, doc, getDoc } from 'firebase/firestore'
 
