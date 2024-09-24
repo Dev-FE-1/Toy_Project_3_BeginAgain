489013a5bf4ab7fd328bb37bb3b1d5b64ac19b3c
김영은 <95126m@naver.com>
2024-09-24 19:51:43 +0900

    bug: db 하나로 합침, 삭제안되는거도 고침

--- src/components/playlist/PlaylistDetail.tsx
+++ src/components/playlist/PlaylistDetail.tsx
@@ -85,22 +85,13 @@ export default function PlaylistDetail({
     const fetchData = async () => {
       try {
         const playlistDocRef = doc(db, 'Playlists', id!)
-        const publicPlaylistDocRef = doc(db, 'PublicPlaylists', id!)
-
-        const [playlistSnap, publicPlaylistSnap] = await Promise.all([
-          getDoc(playlistDocRef),
-          getDoc(publicPlaylistDocRef)
-        ])
-
+  
+        const playlistSnap = await getDoc(playlistDocRef)
+  
         if (playlistSnap.exists()) {
           setPlaylistData({ id: playlistSnap.id, ...playlistSnap.data() })
-        } else if (publicPlaylistSnap.exists()) {
-          setPlaylistData({
-            id: publicPlaylistSnap.id,
-            ...publicPlaylistSnap.data()
-          })
         } else {
-          console.log('No matching documents found in either collection.')
+          console.log('No matching document found in the Playlists collection.')
         }
       } catch (error) {
         console.error('Error fetching playlist data:', error)
@@ -108,7 +99,7 @@ export default function PlaylistDetail({
         setIsLoading(false)
       }
     }
-
+  
     fetchData()
   }, [id, db])
 
